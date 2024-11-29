0ee30718597e15ddfc6997089f6851a7ceb70812
drodrigues21 <davidvilar21@gmail.com>
2024-11-29 20:32:18 +0900

    changed the featured section (need to ask for feedback)

--- src/pages/landing-page.jsx
+++ src/pages/landing-page.jsx
@@ -1,5 +1,13 @@
 import { Carousel, Hero } from "../components/global";
+import "./landing-page.css";
 
+// Images
+import youtubeImage from "../assets/youtube.jpeg";
+import discordImage from "../assets/discord.jpeg";
+import mentorshipImage from "../assets/mentorship.jpeg";
+import eventsImage from "../assets/events.jpeg";
+
+// Component
 export default function Playground() {
 	return (
 		<div>
@@ -18,103 +26,71 @@ export default function Playground() {
 				buttonText="Click Here"
 			/>
 			<Carousel />
+			{/* Featured Projects Header */}
+			<div className="flex flex-col items-center justify-center featured-header">
+				<h2 className="featured-projects-header">
+					We're dedicated to helping you level up in your
+					<span> personal and professional journey.</span>
+				</h2>
+			</div>
 			{/* first row */}
-			<div className="flex w-full flex-col lg:flex-row p-4">
-				<div className="card  rounded-box grid flex-grow place-items-center">
-					<div className="mockup-code">
-						<pre data-prefix="$">
-							<code>npm i letphil</code>
-						</pre>
-						<pre data-prefix=">" className="text-warning">
-							<code>installing...</code>
-						</pre>
-						<pre data-prefix=">" className="text-success">
-							<code>Done!</code>
-						</pre>
-					</div>
+			<div className="featured featured-reverse">
+				<div className="w-full lg:w-1/2 featured-project">
+					<h3 className="featured-project-title">1. YouTube Channel</h3>
+					<p className="featured-project-description">
+						Dive into a wealth of tutorials, tips, and content that helps you
+						learn and grow at your own pace.{" "}
+						<a href="https://www.youtube.com/@letphil">Come check it out!</a>
+					</p>
 				</div>
-				<div className="divider lg:divider-horizontal"></div>
-				<div className="card rounded-box grid  flex-grow place-items-center">
-					<div className="mockup-phone">
-						<div className="camera"></div>
-						<div className="display">
-							<div
-								className="artboard artboard-demo phone-1"
-								style={{
-									width: 260,
-								}}
-							>
-								this video will be up soon! 7pm est and we will share the code
-								too
-							</div>
-						</div>
-					</div>
+				<div className="featured-project-img">
+					<img src={youtubeImage} alt="letphil youtube" />
 				</div>
 			</div>
 			{/* second row */}
-			<div className="flex w-full flex-col lg:flex-row p-4">
-				<div className="card  rounded-box grid flex-grow place-items-center">
-					<div className="mockup-phone">
-						<div className="camera"></div>
-						<div className="display">
-							<div
-								className="artboard artboard-demo phone-1"
-								style={{
-									width: 260,
-								}}
-							>
-								this video will be up soon! 7pm est and we will share the code
-								too
-							</div>
-						</div>
-					</div>
+			<div className="featured">
+				<div className="w-full lg:w-1/2 featured-project">
+					<h3 className="featured-project-title">2. Discord Community</h3>
+					<p className="featured-project-description">
+						Join our vibrant community of 800+ members, where you can network,
+						ask questions, and share your knowledge.{" "}
+						<a
+							href="https://discord.gg/SsWF6cWDRw"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							Join Our Discord Community!
+						</a>
+					</p>
 				</div>
-				<div className="divider lg:divider-horizontal"></div>
-				<div className="card rounded-box grid flex-grow place-items-center">
-					<div className="mockup-code">
-						<pre data-prefix="$">
-							<code>npm i letphil</code>
-						</pre>
-						<pre data-prefix=">" className="text-warning">
-							<code>installing...</code>
-						</pre>
-						<pre data-prefix=">" className="text-success">
-							<code>Done!</code>
-						</pre>
-					</div>
+				<div className="featured-project-img">
+					<img src={discordImage} alt="letphil discord" />
 				</div>
 			</div>
 			{/* third row */}
-			<div className="flex w-full flex-col lg:flex-row p-4">
-				<div className="card  rounded-box grid flex-grow place-items-center">
-					<div className="mockup-code">
-						<pre data-prefix="$">
-							<code>npm i letphil</code>
-						</pre>
-						<pre data-prefix=">" className="text-warning">
-							<code>installing...</code>
-						</pre>
-						<pre data-prefix=">" className="text-success">
-							<code>Done!</code>
-						</pre>
-					</div>
+			<div className="featured featured-reverse">
+				<div className="w-full lg:w-1/2 featured-project">
+					<h3 className="featured-project-title">3. One-on-One Mentorship</h3>
+					<p className="featured-project-description">
+						Get personalized coaching to fast-track your growth, whether you're
+						tackling a new project or overcoming challenges.
+					</p>
 				</div>
-				<div className="divider lg:divider-horizontal"></div>
-				<div className="card rounded-box grid  flex-grow place-items-center">
-					<div className="mockup-phone">
-						<div className="camera"></div>
-						<div className="display">
-							<div
-								className="artboard artboard-demo phone-1"
-								style={{
-									width: 260,
-								}}
-							>
-								this video will be up soon! 7pm est and we will share the code
-								too
-							</div>
-						</div>
-					</div>
+				<div className="featured-project-img">
+					<img src={mentorshipImage} alt="letphil mentorship" />
+				</div>
+			</div>
+			{/* fourth row */}
+			<div className="featured">
+				<div className="w-full lg:w-1/2 featured-project">
+					<h3 className="featured-project-title">4. Community Events</h3>
+					<p className="featured-project-description">
+						Participate in engaging events that bring the community
+						together—workshops, challenges, and more!
+					</p>
+				</div>
+				<div className="featured-project-img">
+					<img src={eventsImage} alt="letphil events" />
 				</div>
 			</div>
 		</div>
