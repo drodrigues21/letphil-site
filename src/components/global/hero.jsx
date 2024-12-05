2d2b81aeab75948460d573e3f8cc6abc9bb32f16
youngsong1230@gmail.com <youngsong1230@gmail.com>
2024-12-04 17:06:46 -0800

    updated hero section

--- src/components/global/hero.jsx
+++ src/components/global/hero.jsx
@@ -6,22 +6,22 @@ export default function Hero({ title, description, buttonText }) {
 
   return (
     <>
-      <div className="min-h-screen bg-slate-50 justify-center items-center flex">
+      <div className="min-h-[82vh] bg-[#f0f4f8] justify-center items-center flex">
         <div className="hero-container">
           <div className="hero-description">
-            <h1>Welcome to the LetPhil Community</h1>
+            <h1>LetPhil Community</h1>
             <p>
               Connect with passionate programmers, learn together, and build a
               brighter coding future!
             </p>
-            <button>Join Now</button>
+            <button className="cta-button">Join Now</button>
           </div>
 
           <div className="video-description">
             <iframe
-              width="550"
+              width="560"
               height="315"
-              src="https://www.youtube.com/embed/z7Ggq5Mzcvw?si=FFwJeEF4XD4ekWD3"
+              src="https://www.youtube.com/embed/ZT9F81rdnT4?si=EaPbpAhAbsFcTlbC"
               title="YouTube video player"
               frameborder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
